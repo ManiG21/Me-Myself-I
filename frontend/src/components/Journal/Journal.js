742079baf9c339cbbcc48f156e544b5addfba1d7
ManiG21 <manigrant023@gmail.com>
2022-10-31 20:25:07 -0400

    form info

--- frontend/src/components/Journal/Journal.js
+++ frontend/src/components/Journal/Journal.js
@@ -7,13 +7,31 @@ export default function Journal() {
     //need a function that submits Journal data to Api 
     const submitData = () => {
      //Send data to api using fetch
+     console.log(data)
+     
     }
     return <div>
+    <form>
+    {/* <input value={employeeName} onChange = {updateEmployeeName}></input> */}
+    {/* title (TEXT) 
+
+entry (TEXT)
+
+color (TEXT)
+
+emoji ( */}
+
+
+        <input value={data.title} onChange = {updateData.title}></input>
+        <input value={data.entry} onChange = {updateData.entry}></input>
+        <input value={data.color} onChange = {updateData.color}></input>
+        <input value={data.emoji} onChange = {updateData.emoji}></input>
+    </form>
     
         <h1>Journal</h1>
         <button onClick={submitData}>Post</button>
     </div>
- //Add emojs
+ //Add emojs using font awesome
  //Add color and Styling 
  //Add input for Title an Entry
  }
