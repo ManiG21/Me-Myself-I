76f4c3b472acd782acc39cb2d359fb914958ca6b
ManiG21 <manigrant023@gmail.com>
2022-11-19 11:39:44 -0500

    NavBar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -3,9 +3,13 @@ import './Navbar.css';
 import logo from './Me Myself & I Logo.gif';
 
 export default function Navbar() {
-    return <div className="nav-button">
-    <Link to="/journal"> <button className="buttonNav">Journal</button></Link>
-    <Link to="/calendar"> <button className="buttonNav">Calendar</button></Link>
-    <div>  <Link to="/"> <img id="logo" src={logo} alt='logo'></img> </Link> </div>
+    return <div className="nav-background">
+        <div className="nav-links">
+    <Link className="logo" to="/"> <img id="logo" src={logo} alt='logo'></img> </Link>
+    <div>
+    <Link to="/journal"> <button id="paths" className="buttonNav button">Journal</button></Link>
+    <Link to="/pie-chart"> <button id="paths" className="buttonNav button">Pie Chart</button></Link>
+    </div>
+    </div>
     </div>
 }
